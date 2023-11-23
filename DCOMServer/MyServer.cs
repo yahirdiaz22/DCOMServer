db7d970985abd671c85e2a03b52d405944618ecb
erick <erick@YAHIR>
2023-11-23 12:48:11 -0600

    Agregar archivos de proyecto.

---
+++ DCOMServer/MyServer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DCOMServer
+{
+    [ComVisible(true)]
+    public interface IMyInterface
+    {
+        string SayHello();
+    }
+
+    [ComVisible(true)]
+    public class MyServer : IMyInterface
+    {
+        public string SayHello()
+        {
+            return "Hola desde el servidor DCOM";
+        }
+    }
+}
+
